5b6b0b2740057f1f39a18653ae2a8e5b51499193
Saubhagya Rai <raisaubhagya@gmail.com>
2021-10-24 13:45:26 +0545

    setting seeder

---
+++ database/seeds/SettingSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Setting;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Setting::create([
+            'phone' => 'phone',
+        'email' => 'email',
+        'address' => 'address',
+        'map' => 'map',
+        'top_ad1_text' => 'top_ad1_text',
+        'top_ad2_text' => 'top_ad2_text',
+        'top_ad3_text' => 'top_ad3_text',
+        'bottom_ad_text' => 'bottom_ad_text',
+
+        'favicon' => 'img.jpg',
+
+        'logo' => 'img.jpg',
+
+        'top_ad1_img' => 'img.jpg',
+
+        'top_ad2_img' => 'img.jpg',
+
+        'top_ad3_img' => 'img.jpg',
+        'bottom_ad_img' => 'img.jpg'
+
+        ]);
+    }
+}
